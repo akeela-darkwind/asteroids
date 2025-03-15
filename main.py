58a7f9c0cca2391fd9441faeb8a61d8310f6e6dd
akeela-darkwind <nikiebcpht@gmail.com>
2025-03-15 11:40:23 +0100

    implemented framerate limit

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -16,6 +18,10 @@ def main():
             
         screen.fill("black")
         pygame.display.flip()
+
+        #limit framerate to 60 FPS
+        clock.tick(60) / 1000
+
 
 
 
