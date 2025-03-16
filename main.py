43d65faf43795647fecf731a309a48482275b6b4
akeela-darkwind <nikiebcpht@gmail.com>
2025-03-16 17:32:13 +0100

    added movement to player avatar

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from constants import *
 from player import Player
 
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -9,26 +10,19 @@ def main():
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
-    #print("Starting Asteroids!")
-    #print(f"Screen width: {SCREEN_WIDTH}")
-    #print(f"Screen height: {SCREEN_HEIGHT}")
-
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        player.update(dt)    
             
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
 
         #limit framerate to 60 FPS
-        clock.tick(60) / 1000
-
-        
-
-
-
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
